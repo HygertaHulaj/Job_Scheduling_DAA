b6d5878943b217cf7386703392c863623c973b35
AlbinDemukaj <94542503+AlbinDemukaj@users.noreply.github.com>
2023-05-17 17:40:00 +0200

    Update job_scheduling.py

--- job_scheduling.py
+++ job_scheduling.py
@@ -8,3 +8,8 @@ def printjobschedule(jobs):
     for job in jobs:
         if job[2]:
             graph[job[2]].append(job[0])
+        else:
+            graph[None].append(job[0])
+
+    # Sort jobs by their deadlines in increasing order
+    jobs = sorted(jobs, key=lambda x: x[1])
